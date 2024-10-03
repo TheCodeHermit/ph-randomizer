15c3da2e4aceea9ec31c92b5fb909b88e04cdb6e
TheCodeHermit <42543560+TheCodeHermit@users.noreply.github.com>
2024-10-03 11:15:12 -0600

    Create fakecli.py

---
+++ ph_rando/ui/fakecli.py
@@ -0,0 +1,67 @@
+import json
+import logging
+from pathlib import Path
+
+import click
+
+from ph_rando.common import click_setting_options
+from ph_rando.patcher._patcher import Patcher
+from ph_rando.shuffler._shuffler import Shuffler
+from ph_rando.shuffler._spoiler_log import generate_spoiler_log
+from ph_rando.shuffler._util import generate_random_seed
+
+
+@click.command()
+@click.option(
+    '-i',
+    '--input-rom-path',
+    required=False,
+    type=click.Path(exists=True, path_type=Path),
+    help='Path to stock ROM to randomize.',
+)
+@click.option(
+    '-o',
+    '--output-rom-path',
+    type=click.Path(exists=False, path_type=Path),
+    required=False,
+    help='Path to save randomized ROM to.',
+)
+@click.option(
+    '--spoiler-log',
+    required=False,
+    type=click.Path(exists=False, path_type=Path),
+    help='File path to save spoiler log to.',
+)
+@click.option('-s', '--seed', type=str, required=False, help='Seed for the randomizer.')
+@click.option(
+    '-l',
+    '--log-level',
+    type=click.Choice(
+        list(logging.getLevelNamesMapping().keys()),
+        case_sensitive=False,
+    ),
+    default='INFO',
+)
+@click_setting_options
+def randomizer_cli(
+    spoiler_log: Path | None,
+    seed: str | None,
+    log_level: str,
+    **settings: bool | str | set[str],
+) -> None:
+    logging.basicConfig(level=logging.getLevelNamesMapping()[log_level])
+
+    # Generate random seed if one isn't provided
+    if seed is None:
+        seed = generate_random_seed()
+
+    # Run the shuffler
+    shuffled_aux_data = Shuffler(seed, settings).generate()
+
+    if spoiler_log:
+        sl = generate_spoiler_log(shuffled_aux_data, settings).dict()
+        Path(spoiler_log).write_text(json.dumps(sl, indent=2))
+
+
+if __name__ == '__main__':
+    randomizer_cli()
